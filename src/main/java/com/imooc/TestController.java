b51d8b8dbe7333e6eb07a1d5f1137e2097e3713e
taoxudong <taoxudong@oraro.net>
2019-09-06 15:39:12 +0800

    init
    Change-Id: I4d4678cdd3080a57d9d3a4a181a123eea9f9467d

---
+++ src/main/java/com/imooc/TestController.java
@@ -0,0 +1,16 @@
+package com.imooc;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping("/get1")
+    private ResultBean get1(){
+        System.out.println("TestController.get1()");
+        return new ResultBean("get1 OK");
+    }
+}
